bf6a4ec3b1a04b84999f360fb5a349d47c2dc06c
Jieke Choo <jiekechoo@gmail.com>
2022-01-10 11:06:13 +0800

    修改 yiqisoft-logo oauth2 登陆标签

--- ui-ngx/src/app/app.component.ts
+++ ui-ngx/src/app/app.component.ts
@@ -1,20 +1,20 @@
 
-import 'hammerjs';
-
 import { Component, OnInit } from '@angular/core';
-
-import { environment as env } from '@env/environment';
-
-import { TranslateService } from '@ngx-translate/core';
-import { select, Store } from '@ngrx/store';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { selectIsAuthenticated, selectIsUserLoaded } from '@core/auth/auth.selectors';
+import { AuthService } from '@core/auth/auth.service';
 import { AppState } from '@core/core.state';
 import { LocalStorageService } from '@core/local-storage/local-storage.service';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
+import { environment as env } from '@env/environment';
+import { select, Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import 'hammerjs';
 import { combineLatest } from 'rxjs';
-import { selectIsAuthenticated, selectIsUserLoaded } from '@core/auth/auth.selectors';
 import { distinctUntilChanged, filter, map, skip } from 'rxjs/operators';
-import { AuthService } from '@core/auth/auth.service';
+
+
+
 
 @Component({
   selector: 'tb-root',
@@ -60,6 +60,13 @@ export class AppComponent implements OnInit {
       'apple-logo',
       this.domSanitizer.bypassSecurityTrustHtml(
         '<svg viewBox="0 0 256 315"><path d="M213.803394,167.030943 C214.2452,214.609646 255.542482,230.442639 256,230.644727 C255.650812,231.761357 249.401383,253.208293 234.24263,275.361446 C221.138555,294.513969 207.538253,313.596333 186.113759,313.991545 C165.062051,314.379442 158.292752,301.507828 134.22469,301.507828 C110.163898,301.507828 102.642899,313.596301 82.7151126,314.379442 C62.0350407,315.16201 46.2873831,293.668525 33.0744079,274.586162 C6.07529317,235.552544 -14.5576169,164.286328 13.147166,116.18047 C26.9103111,92.2909053 51.5060917,77.1630356 78.2026125,76.7751096 C98.5099145,76.3877456 117.677594,90.4371851 130.091705,90.4371851 C142.497945,90.4371851 165.790755,73.5415029 190.277627,76.0228474 C200.528668,76.4495055 229.303509,80.1636878 247.780625,107.209389 C246.291825,108.132333 213.44635,127.253405 213.803394,167.030988 M174.239142,50.1987033 C185.218331,36.9088319 192.607958,18.4081019 190.591988,0 C174.766312,0.636050225 155.629514,10.5457909 144.278109,23.8283506 C134.10507,35.5906758 125.195775,54.4170275 127.599657,72.4607932 C145.239231,73.8255433 163.259413,63.4970262 174.239142,50.1987249" fill="#000000"></path></svg>'
+      )
+    );
+
+    this.matIconRegistry.addSvgIconLiteral(
+      'yiqisoft-logo',
+      this.domSanitizer.bypassSecurityTrustHtml(
+        '<svg viewbox="0 0 200 200"><g stroke-width="12"><path stroke="#333" fill="none" d="M100 91v70M55.216 28.019 93.784 73.98m50.178-45.078-37.924 45.196"/><circle cy="81" cx="100" stroke="red" fill="#fff" r="12"/><circle cy="21" cx="50" stroke="#005fbf" fill="#fff" r="12"/><circle cy="21" cx="150" stroke="#00bf00" fill="#fff" r="12"/><circle cy="173" cx="100" stroke="#ff7f00" fill="#fff" r="12"/></g></svg>'
       )
     );
 
